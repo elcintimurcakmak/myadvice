b3683e70af500205e563dd6a7ea3f8bef5b5193e
Dr. Elçin TİMUR ÇAKMAK <75584540+elcintimurcakmak@users.noreply.github.com>
2023-05-23 13:46:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 500)
 pd.set_option('display.float_format', lambda x: '%.0f' % x)
 
-df = pd.read_excel("hotel_info.xlsx")
-hotel = pd.read_excel("hotel_info.xlsx", sheet_name="hotel")
-ratings = pd.read_excel("hotel_info.xlsx", sheet_name="ratings")
+df = pd.read_excel("C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/hotel_info.xlsx")
+hotel = pd.read_excel("C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/hotel_info.xlsx", sheet_name="hotel")
+ratings = pd.read_excel("C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/hotel_info.xlsx", sheet_name="ratings")
 df_ = pd.merge(hotel, ratings, how="left", on=["hotel_id"])
 df = df_.copy()
 df.head()
@@ -34,15 +34,14 @@ tabs = ["Page_1"]
 
 #SIDEBAR MYADVICE LOGOSU EKLEME
 from PIL import Image
-image_path = "C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/myadvice_logo_tranp_22.png"
+image_path = "C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/nese7.png"
 image = Image.open(image_path)
 st.sidebar.image(image, use_column_width=True)
 
 
-
 #QUICK SEARCH YAZISI
 #st.sidebar.markdown("<h1 style='text-align:center;'>QUICK SEARCH</h1>", unsafe_allow_html=True)
-image_path = "C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/quicksearch2.png"
+image_path = "C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/quick_search_nese.jpg"
 image = Image.open(image_path)
 st.sidebar.image(image, use_column_width=True)
 
@@ -113,7 +112,7 @@ from PIL import Image
 #SLOGAN FOTOĞRAFI
 def main():
     # Fotoğraf ekleme örneği
-    image_path = "C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/banner.png"
+    image_path = "C:/Users/Lenovo/PycharmProjects/pythonProject/MY_LUDW_PROJECT/streamlit_canli/myadvice_nese.jpg"
     image = Image.open(image_path)
     #st.image(image, caption='Ludwigsburg', use_column_width=True)
     st.image(image, use_column_width=True)
@@ -129,42 +128,128 @@ def main():
     #st.image(image, caption='Ludwigsburg', use_column_width=True)
     st.image(image, use_column_width=True)
 
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
+
+lake_view = True
+city_view = True
+hotel_type = True
+guest_house_type = True
+credit_card = True
+payment_refund = True
+restaurant = True
+non_smoking = True
+fitness = True
+pet = True
 
 
-# breakfast
+########################################################################################
+########################################################################################
+########################################################################################
+########################################################################################
+
+# Başlangıçta tüm otelleri önerilen otellere atayın
+recommended_hotels = df.copy()
+st.subheader("ADVICED FEATURES")
+# breakfast seçeneklerine göre filtreleme
 if no_breakfast:
-    recommended_hotels = df[df["breakfast"] == 1]
-else:
-    recommended_hotels = df
+    recommended_hotels = recommended_hotels[recommended_hotels["breakfast"] == 0]
+if american_breakfast:
+    recommended_hotels = recommended_hotels[recommended_hotels["breakfast_american"] == 1]
+if continental_breakfast:
+    recommended_hotels = recommended_hotels[recommended_hotels["breakfast_continental"] == 1]
+if vegeterian_breakfast:
+    recommended_hotels = recommended_hotels[recommended_hotels["breakfast_vegetarian"] == 1]
+if glutenfree_breakfast:
+    recommended_hotels = recommended_hotels[recommended_hotels["breakfast_glutenfree"] == 1]
+if openbuffet_breakfast:
+    recommended_hotels = recommended_hotels[recommended_hotels["breakfast_openbuffet"] == 1]
+if takeaway_breakfast:
+    recommended_hotels = recommended_hotels[recommended_hotels["takeaway_breakfast"] == 1]
+
+# Aynı otelleri kaldırın ve benzersiz otelleri getirin
+recommended_hotels = recommended_hotels.drop_duplicates(subset=["hotel_name_x"])
 
 # Ratinge göre sıralama ve ilk 5 oteli getirme
-df = recommended_hotels.sort_values("hotel_star", ascending=False).head()
-
-# Benzersiz otelleri getirme
-df.drop_duplicates(subset=['hotel_name_x'], inplace=True)
-
-#df['hotel_star'] = df['hotel_star'].astype(int)
-df['weekdays_price'] = df['weekdays_price'].astype(int)
+recommended_hotels = recommended_hotels.sort_values("reviews", ascending=False).head(5)
 
 
-# Öneri yapma
-st.subheader("ADVISED ACCOMMODATION")
-for _, hotel in df.iterrows():
-    col1, col2, col3, col4, col5 = st.columns([2,1,1,1,1])
 
-    col1.markdown(f"<b>NAME:</b> {hotel['hotel_name_x']}", unsafe_allow_html=True)
+
+
+####################################################################
+##################OTEL DETAYLARI FOTOLU#############################
+####################################################################
+# Tek bir otel önerisi yerine tüm önerilen otelleri gösterin
+
+for index, hotel in recommended_hotels.iterrows():
+    #st.write(hotel["hotel_name_x"])
+    col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1, 1])
+
+    hotel_name = hotel['hotel_name_x'].upper()
+    col1.markdown(f"<b>NAME:</b> {hotel_name}", unsafe_allow_html=True)
+    # col1.markdown(f"<b>NAME:</b> {hotel['hotel_name_x']}", unsafe_allow_html=True)
 
     image_path = hotel['hotel_star_icon']
     if os.path.exists(image_path):
         image = Image.open(image_path)
         col2.image(image, use_column_width=False)
-        #col2.image(image, caption=hotel['hotel_star_icon'], use_column_width=False)
+    col3.markdown(f'<a href="{hotel["embed"]}">Show on map</a>', unsafe_allow_html=True)
+    st.markdown(f"<b>PRICE (per night/€):</b> {hotel['weekdays_price']}", unsafe_allow_html=True)
+    #st.markdown(f"<b>STAR:</b> {hotel['hotel_star']}", unsafe_allow_html=True)
+    st.markdown(f"<b>DESCRIPTION:</b> {hotel['hotel_description']}", unsafe_allow_html=True)
 
+    image_path = hotel['hotel_image']
+    if os.path.exists(image_path):
+        image = Image.open(image_path)
+        st.image(image, caption=hotel['hotel_name_x'], use_column_width=True)
+    else:
+        st.write("Görüntü bulunamadı.")
+    st.write("---")
+########################################################################################
+########################################################################################
+########################################################################################
+########################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Öneri yapma
+st.subheader("ADVICED FEATURES")
+for _, hotel in df.iterrows():
+    col1, col2, col3, col4, col5 = st.columns([2,1,1,1,1])
+
+    hotel_name = hotel['hotel_name_x'].upper()
+    col1.markdown(f"<b>NAME:</b> {hotel_name}", unsafe_allow_html=True)
+    #col1.markdown(f"<b>NAME:</b> {hotel['hotel_name_x']}", unsafe_allow_html=True)
+
+    image_path = hotel['hotel_star_icon']
+    if os.path.exists(image_path):
+        image = Image.open(image_path)
+        col2.image(image, use_column_width=False)
     col3.markdown(f'<a href="{hotel["embed"]}">Show on map</a>', unsafe_allow_html=True)
 
 
+
+    # number_of_reviews değişkenini link olarak görüntüleyin
+
+    #col4.markdown(f"{hotel['number_of_reviews']} reviews", unsafe_allow_html=True)
+    #col4.markdown(f"<a href='#{hotel['reviews']}' target='_blank'>{hotel['number_of_reviews']} reviews</a>", unsafe_allow_html=True)
+
+########################
+
+    ########################
     st.markdown(f"<b>PRICE (per night/€):</b> {hotel['weekdays_price']}", unsafe_allow_html=True)
     #st.markdown(f"<b>STAR:</b> {hotel['hotel_star']}", unsafe_allow_html=True)
     st.markdown(f"<b>DESCRIPTION:</b> {hotel['hotel_description']}", unsafe_allow_html=True)
@@ -184,15 +269,15 @@ for _, hotel in df.iterrows():
 #FACILITIES
 st.sidebar.write("<h3>Facilities</h3>", unsafe_allow_html=True)
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Restaurant")
+restaurant = col1.checkbox("Restaurant")
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Bar")
+bar = col1.checkbox("Bar")
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Non-Smoking")
+non_smoking = col1.checkbox("Non-Smoking")
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Fitness")
+fitness = col1.checkbox("Fitness")
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Pet")
+pet = col1.checkbox("Pet")
 st.sidebar.write("", unsafe_allow_html=True)
 
 
@@ -208,11 +293,9 @@ st.sidebar.write("<h3>Business Types</h3>", unsafe_allow_html=True)
 #col1.image(image, use_column_width=True)
 
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Hotel")
+hotel_type = col1.checkbox("Hotel")
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Pension")
-col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Guest House")
+guest_house_type = col1.checkbox("Guest House")
 col1, col2  = st.sidebar.columns([5,2])
 st.sidebar.write("", unsafe_allow_html=True)
 
@@ -222,25 +305,20 @@ st.sidebar.write("", unsafe_allow_html=True)
 #PAYMENT METHOD
 st.sidebar.write("<h3>Payment</h3>", unsafe_allow_html=True)
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Credit Card")
+credit_card = col1.checkbox("Credit Card")
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Payment Refund")
+payment_refund = col1.checkbox("Payment Refund")
 st.sidebar.write("", unsafe_allow_html=True)
 
 #ROOM VIEW
 st.sidebar.write("<h3>Room View</h3>", unsafe_allow_html=True)
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Garden View")
+garden_view = col1.checkbox("Garden View")
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("Lake View")
+lake_view = col1.checkbox("Lake View")
 col1, col2  = st.sidebar.columns([5,2])
-checked = col1.checkbox("City View")
+city_view = col1.checkbox("City View")
 st.sidebar.write("", unsafe_allow_html=True)
-
-
-
-
-
 
 
 
@@ -262,8 +340,7 @@ st.sidebar.write("", unsafe_allow_html=True)
 #st.write("""3. Tahminleme Sayfası""")
 
 
-import streamlit as st
-print(st.__version__)
+
 
 
 
